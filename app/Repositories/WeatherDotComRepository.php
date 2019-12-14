4e79b8dc5018ef07fa5f877258cdc3f4533df6f3
Amir <amircup2006@gmail.com>
2019-12-14 16:51:51 +0330

    Implementing the functionality of weather.com repository

--- app/Repositories/WeatherDotComRepository.php
+++ app/Repositories/WeatherDotComRepository.php
@@ -1,18 +1,57 @@
 <?php
 namespace App\Repositories;
 
-use App\Contracts\DegreeInterface;
 use App\DataContracts\DegreeItemDTO;
-use App\Services\Degrees\Fahrenheit;
-use Illuminate\Support\Collection;
+use App\Mocks\WeatherDotComMock;
+use App\Services\CSV;
+use App\Services\Degrees\Celsius;
 
 class WeatherDotComRepository extends AbstractWeatherProviderRepository
 {
+    private $rawData;
+
+    /**
+     * @var array
+     */
+    private $dataArray;
+
     /**
      * @return DegreeItemDTO[]
      */
     public function degreeOfDay(): array
     {
-        return new Fahrenheit(10);
+        $this->getFromSource();
+        $this->collectData();
+
+        return $this->decorateData();
+    }
+
+    private function getFromSource()
+    {
+        $data = new WeatherDotComMock($this->city, $this->date);
+        $this->rawData = $data->serveAsCSV();
+    }
+
+    private function collectData()
+    {
+        $csv = (new CSV($this->rawData));
+        $this->dataArray = $csv->toArray();
+    }
+
+    /**
+     * @return DegreeItemDTO[]
+     */
+    private function decorateData()
+    {
+        $degreeItemDTO = [];
+        foreach ($this->dataArray as $item){
+            $DTO = new DegreeItemDTO();
+            $DTO->hour = (int)str_replace(':00', '', $item['prediction__time']);
+            $DTO->degree = new Celsius($item['prediction__value']);
+
+            $degreeItemDTO[] = $DTO;
+        }
+
+        return $degreeItemDTO;
     }
 }
