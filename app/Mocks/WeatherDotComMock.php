849a8560edc822ccea6d521fa029eb9bd583138e
Amir <amircup2006@gmail.com>
2019-12-14 16:51:24 +0330

    Creating and implementing the mocker class of weather.com

---
+++ app/Mocks/WeatherDotComMock.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mocks;
+
+use Carbon\Carbon;
+
+class WeatherDotComMock
+{
+    private $date;
+
+    public function __construct(string $city, Carbon $date)
+    {
+        $this->date = $date;
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function serveAsCSV()
+    {
+        return $this->getRawData();
+    }
+
+    /**
+     * @return string
+     */
+    private function getRawData()
+    {
+        return "\"-scale\",\"city\",\"date\",\"prediction__time\",\"prediction__value\"
+\"celsius\",\"{$this->city}\",\"{$this->date->format('Ymd')}\",\"00:00\",\"05\"
+\"\",\"\",\"\",\"01:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"02:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"03:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"04:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"05:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"06:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"07:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"08:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"09:00\",\"".rand(5,10)."\"
+\"\",\"\",\"\",\"10:00\",\"".rand(5,10)."\"
+";
+    }
+}
