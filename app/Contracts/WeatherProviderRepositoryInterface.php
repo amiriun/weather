469db32d6648154bc85799d479965ef009767fb1
Amir <amircup2006@gmail.com>
2019-12-15 16:26:59 +0330

    Polishing

--- app/Contracts/WeatherProviderRepositoryInterface.php
+++ app/Contracts/WeatherProviderRepositoryInterface.php
@@ -3,7 +3,15 @@
 namespace App\Contracts;
 
 use App\DataContracts\DegreeItemDTO;
+use Carbon\Carbon;
 
+/**
+ * Interface WeatherProviderRepositoryInterface
+ *
+ * @package App\Contracts
+ * @property string city
+ * @property Carbon date
+ */
 interface WeatherProviderRepositoryInterface
 {
     /**
